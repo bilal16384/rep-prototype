662aa1d307272fd6d1b54feead1294b6c1c3a514
Bilal Bertolini <bilal.bertolini@edu.gbjb.ch>
2026-05-03 19:20:26 +0200

    ajout d'un script permettant de déplacer le personnage horizontalement

--- prototype-unity/Assets/scripts/script_move.cs
+++ prototype-unity/Assets/scripts/script_move.cs
@@ -1,16 +1,30 @@
+using Unity.Collections;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class script_move : MonoBehaviour
 {
+    
+    
+    
+    
+    public float vitesse;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
-
     // Update is called once per frame
     void Update()
     {
+        Keyboard clavier = Keyboard.current;
         
+        if (clavier != null)
+        {
+            float moveX = 0;
+            if (clavier.dKey.isPressed) moveX = 1;
+            if (clavier.aKey.isPressed) moveX = -1;
+            
+            transform.position += new Vector3(moveX, 0, 0) * 5f * Time.deltaTime;
+        }
     }
 }
