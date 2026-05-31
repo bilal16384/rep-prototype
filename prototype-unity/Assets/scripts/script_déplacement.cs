be2c68c8086ec5f257d6f335ceeb36c7d9a17a7f
Bilal Bertolini <bilal.bertolini@edu.gbjb.ch>
2026-05-31 17:46:35 +0200

    Correction de 2 bugs sur les personnages

--- prototype-unity/Assets/scripts/script_déplacement.cs
+++ prototype-unity/Assets/scripts/script_déplacement.cs
@@ -11,17 +11,30 @@ public class script_move : MonoBehaviour
     
     
     
-    public float vitesse;
+
 
     private Rigidbody2D rb;
-
     private BoxCollider2D boxCollider;
 
-    private bool regardeDroite = true;
 
+
+
+    private bool regardeDroite = true;
+    private float longueurSaut;
+    private bool estEnSaut = false;
+
+
+
+    //touches
     public Key toucheDroite = Key.D;
     public Key toucheGauche = Key.A;
     
+    
+    
+    public float valeur_longueurSaut;
+    public Key toucheSaut = Key.W;
+    public float forceSaut;
+    public float vitesse;
 
 
 
@@ -31,6 +44,8 @@ public class script_move : MonoBehaviour
     {
         transform.position = new UnityEngine.Vector3(0, 0, 0);
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        longueurSaut = valeur_longueurSaut;
     }
     // Update is called once per frame
     void Update()
@@ -64,10 +79,56 @@ public class script_move : MonoBehaviour
                 regardeDroite = false;
                 transform.localScale = new UnityEngine.Vector3(-1, 1, 1);
             }
-        }    
-            
+
+
+
+
+            // saut
+            if (clavier[toucheSaut].wasPressedThisFrame && detectersol() == true)
+            {
+                rb.linearVelocity = new UnityEngine.Vector2(rb.linearVelocity.x, forceSaut);
+                estEnSaut = true;
+                
+            }
+
+            if (estEnSaut == true && clavier [toucheSaut].isPressed)
+            {
+                rb.linearVelocity = new UnityEngine.Vector2(rb.linearVelocity.x, forceSaut);
+                longueurSaut -= Time.deltaTime;
+                if (longueurSaut <= 0)
+                {
+                    estEnSaut = false;
+                    longueurSaut = valeur_longueurSaut;
+                
+                }
+            }
         
-
-
+            else if (estEnSaut == true && clavier[toucheSaut].wasReleasedThisFrame)
+            {
+                estEnSaut = false;
+                longueurSaut = valeur_longueurSaut;
+            }
+        }    
     }
+        
+    bool detectersol()
+    {
+        UnityEngine.Vector2 tailleReduite = new UnityEngine.Vector2(boxCollider.bounds.size.x * 0.8f, boxCollider.bounds.size.y);
+        
+        RaycastHit2D detecterSol = Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            tailleReduite,
+            0f,
+            UnityEngine.Vector2.down,
+            0.1f,
+            UnityEngine.LayerMask.GetMask("Terrain")
+        );
+        if (detecterSol.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
 }
